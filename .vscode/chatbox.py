7f497c8194ddba2d8d3df3fb2e7ec6e00d2a0f41
tamreen <tamreen2005@gmail.com>
2024-02-13 22:23:50 +0400

    practise.py

---
+++ .vscode/chatbox.py
@@ -0,0 +1,12 @@
+"""
+This program is a chatbox
+takes in name and enquiry
+
+"""
+
+print("thank you")
+#an input of what type of help is required is expected
+name=input("before starting the survey, please enter your name")
+'''name is passed '''
+print("thank you for ur enquiry",name,"we'll get back to you shortly")
+print(__doc__)
